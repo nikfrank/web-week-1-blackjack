0b586671fe42ade1011385f8a85cb09b8833e000
nik frank <nikbfrank@gmail.com>
2019-07-09 05:14:54 -0700

    built the entire game up to doubling down

--- src/App.js
+++ src/App.js
@@ -1,26 +1,256 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Hand } from 'react-deck-o-cards';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const dealCard = ()=> ({
+  rank: Math.floor( Math.random()*13 ) + 1,
+  suit: Math.floor( Math.random()*4 ),
+});
+
+const handValue = hand=>{
+  let total = 0;
+  let hasAce = false;
+
+  for(let i=0; i<(hand.length); i++){
+    if( hand[i].rank === 1 ) hasAce = true;
+    if(hand[i].rank > 10){
+      total += 10;
+    } else {
+      total += hand[i].rank;
+    }
+  }
+
+  if( hasAce && (total < 12) ) total += 10;
+
+  return total;
+};
+
+const dealerAction = hand=> {
+  let total = 0;
+  let hasAce = false;
+
+  for(let i=0; i<(hand.length); i++){
+    if( hand[i].rank === 1 ) hasAce = true;
+    if(hand[i].rank > 10){
+      total += 10;
+    } else {
+      total += hand[i].rank;
+    }
+  }
+
+  // soft seventeen
+  if( hasAce && (total === 7) ) return 'hit';
+
+  if( hasAce && (total < 12) ) total += 10;
+
+  if( total >= 17 ) return 'stay';
+  else return 'hit';
+};
+
+
+const defHandStyle = {
+  maxHeight:'34vh',
+  minHeight:'34vh',
+
+  maxWidth:'100vw',
+  padding: 0,
+};
+
+const MINIMUM_BET = 100;
+
+class App extends React.Component {
+  state = {
+    hand: [],
+    dealerHand: [],
+    wallet: 1000,
+    amountToBet: MINIMUM_BET,
+    bet: 0,
+    hasPlayerStayed: false,
+  }
+
+  bet = ()=> {
+    if( (this.state.bet === 0) &&
+        (this.state.amountToBet >= MINIMUM_BET) &&
+        (this.state.wallet >= this.state.bet) )
+      this.setState({
+        wallet: this.state.wallet - this.state.amountToBet,
+        bet: this.state.amountToBet,
+        hand: [dealCard(), dealCard()],
+        dealerHand: [dealCard()],
+      });
+  }
+
+  allIn = ()=> {
+    this.setState({
+      amountToBet: this.state.wallet,
+    });
+  }
+
+  setAmount = event=> {
+    this.setState({
+      amountToBet: Math.max(
+        Math.min(1*event.target.value, this.state.wallet),
+        MINIMUM_BET
+      ),
+    });
+  }
+
+  hit = ()=> {
+    const newCard = dealCard();
+    this.setState({
+      hand: [...this.state.hand, newCard],
+    }, ()=> {
+      const total = handValue(this.state.hand);
+      if(total > 21){
+        this.lose();
+      }
+    });
+  }
+
+  doubleDown = ()=>{
+    const newCard = dealCard();
+    this.setState({
+      hand: [...this.state.hand, newCard],
+      bet: 2* this.state.bet,
+      wallet: this.state.wallet - this.state.bet,
+    }, ()=> {
+      const total = handValue(this.state.hand);
+      if(total > 21){
+        this.lose();
+      } else {
+        this.stay();
+      }
+    });
+  }
+
+  stay = ()=> this.setState({
+    hasPlayerStayed: true,
+  }, ()=> this.dealerHit())
+
+  dealerHit = ()=> {
+    const nextCard = dealCard();
+    const nextDealerHand = [...this.state.dealerHand, nextCard];
+    const nextAction = dealerAction(nextDealerHand);
+
+    if( nextAction === 'stay' ){
+      let dealerValue = handValue(nextDealerHand);
+      let playerValue = handValue(this.state.hand);
+      if( ((dealerValue > playerValue) && (dealerValue <= 21)) || (
+        playerValue > 21
+      ) ){
+        // lose
+        this.lose();
+
+      } else if( dealerValue === playerValue ){
+        // push
+        this.push();
+
+      } else if( playerValue <= 21 ) {
+        // win
+        if( (playerValue === 21) && (this.state.hand.length === 2)){
+          this.blackjack();
+        } else {
+          this.win();
+        }
+      } else console.error('this code should not run');
+
+      this.setState({
+        dealerHand: nextDealerHand
+      });
+    } else {
+      // dealer hits.
+      this.setState({
+        dealerHand: nextDealerHand
+      }, ()=> setTimeout(()=> this.dealerHit(), 1000));
+    }
+  }
+
+  lose = ()=> {
+    setTimeout(()=>
+      this.setState({
+        bet: 0,
+        hand: [],
+        dealerHand: [],
+        hasPlayerStayed: false,
+      }), 3000);
+
+    toast.error('YOU LOSE YOU LOSER');
+  }
+
+  push = ()=> {
+    setTimeout(()=>
+      this.setState({
+        wallet: this.state.wallet + this.state.bet,
+        bet: 0,
+        hand: [],
+        dealerHand: [],
+        hasPlayerStayed: false,
+      }), 3000);
+
+    toast.warn('that was close...');
+  }
+
+  win = ()=> {
+    setTimeout(()=>
+      this.setState({
+        wallet: this.state.wallet + 2*this.state.bet,
+        bet: 0,
+        hand: [],
+        dealerHand: [],
+        hasPlayerStayed: false,
+      }), 3000);
+
+    toast.info('I LIKE MONEY');
+  }
+
+  blackjack = ()=> {
+    setTimeout(()=>
+      this.setState({
+        wallet: this.state.wallet + 2.5*this.state.bet,
+        bet: 0,
+        hand: [],
+        dealerHand: [],
+        hasPlayerStayed: false,
+      }), 3000);
+
+    toast.success('BLACKJACK you loser');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.bet}>BET</button>
+        <input type='number'
+               onChange={this.setAmount}
+               value={this.state.amountToBet} />
+        <button onClick={this.allIn}>ALL IN</button>
+
+        <div className='wallet'>Wallet: ${this.state.wallet}</div>
+        <div className='bet'>Current Bet: ${this.state.bet}</div>
+        <Hand cards={this.state.dealerHand}
+              hidden={false} style={defHandStyle} />
+        <Hand cards={this.state.hand}
+              hidden={false} style={defHandStyle} />
+
+        {((this.state.bet >= MINIMUM_BET)&&(!this.state.hasPlayerStayed)) ? (
+          <>
+            <button onClick={this.hit}>HIT</button>
+            <button onClick={this.stay}>STAY</button>
+            {this.state.hand.length === 2 ? (
+              <button onClick={this.doubleDown}
+                      disabled={this.state.wallet < this.state.bet}>
+                DOUBLE DOWN
+              </button>
+            ):(null)}
+          </>
+        ): null }
+        <ToastContainer autoClose={3000}/>
+      </div>
+    );
+  }
 }
 
 export default App;
